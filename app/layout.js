03c242d1eca084928778fe382ddfd4bc37eb941b
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2024-08-31 22:06:44 +0300

    Add starter files

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Logo from './copmonents/Logo';
 import Navigation from './copmonents/Navigation';
 
 export const metadata = {
@@ -8,6 +9,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="eng">
       <body>
+        <header>
+          <Logo />
+        </header>
         <Navigation />
         <main>{children}</main>
         <footer>Copyright by The Wild Oasis</footer>
