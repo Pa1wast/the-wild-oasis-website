61485e4f9d4653ba690f02d60db75e18c46290b5
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2024-08-31 12:39:34 +0300

    Update

--- app/page.js
+++ app/page.js
@@ -1,3 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <h1>The Wild Oasis. Welcome to paadise!</h1>;
+  return (
+    <div>
+      <h1>The Wild Oasis. Welcome to paadise!</h1>
+
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
