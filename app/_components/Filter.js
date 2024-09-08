21ccf062ccec941b2fb5ef2f1623f069933771e3
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2024-09-08 17:34:15 +0300

    Update

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -51,14 +51,17 @@ function Filter() {
       <Button filter="all" handleFilter={handleFilter} activeFilter={activeFilter}>
         All cabins
       </Button>
+
       <Button filter="small" handleFilter={handleFilter} activeFilter={activeFilter}>
-        1&smdash;3 guests
+        1-3 guests
       </Button>
+
       <Button filter="medium" handleFilter={handleFilter} activeFilter={activeFilter}>
-        4&smdash;7 guests
+        4-7 guests
       </Button>
+
       <Button filter="large" handleFilter={handleFilter} activeFilter={activeFilter}>
-        8&smdash;12 guests
+        8-12 guests
       </Button>
     </div>
   );
@@ -68,7 +71,7 @@ function Button({ filter, handleFilter, activeFilter, children }) {
   return (
     <button
       className={`px-5 py-2 hover:bg-primary-700 ${
-        activeFilter === filter.value ? 'bg-primary-700' : ''
+        activeFilter === filter ? 'bg-primary-700 text-primary-50' : ''
       }`}
       onClick={() => handleFilter(filter)}
     >
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -16,6 +16,7 @@ export default function Page({ searchParams }) {
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">Our Luxury Cabins</h1>
+
       <p className="text-primary-200 text-lg mb-10">
         Cozy yet luxurious cabins, located right in the heart of the Italian Dolomites. Imagine
         waking up to beautiful mountain views, spending your days exploring the dark forests around,
