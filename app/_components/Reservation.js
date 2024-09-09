8ab633a41fdc3f7cc29396178fdf366e0157d8c8
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2024-09-09 15:52:09 +0300

    Update

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,17 +1,20 @@
 import { getBookedDatesByCabinId, getSettings } from '@/app/_lib/data-service';
 import DateSelector from '@/app/_components/DateSelector';
 import ReservationForm from '@/app/_components/ReservationForm';
+import LoginMessage from './LoginMessage';
+import { auth } from '../_lib/auth';
 
 async function Reservation({ cabin }) {
-  const [settings, bookedDates] = await Promise.all([
+  const [settings, bookedDates, session] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
+    auth(),
   ]);
 
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector settings={settings} bookedDates={bookedDates} cabin={cabin} />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? <ReservationForm cabin={cabin} user={session.user} /> : <LoginMessage />}
     </div>
   );
 }
