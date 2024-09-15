7e6fd75967a1ed70567cce19e05134aa13525a09
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2024-09-15 18:13:39 +0300

    Update

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from 'next/cache';
 import { auth, signIn, signOut } from './auth';
 import { supabase } from './supabase';
 import { getBookings } from './data-service';
+import { redirect } from 'next/navigation';
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -28,30 +29,36 @@ export async function updateGuest(formData) {
   revalidatePath('/account/profile');
 }
 
-export async function editReservation(updatedFields) {
+export async function updateBooking(formData) {
   const session = await auth();
 
   if (!session) throw new Error('You must be logged in');
+
+  const bookingId = Number(formData.get('bookingId'));
 
   const guestBookings = await getBookings(session.user.guestId);
   const guestBookingsIds = guestBookings.map(booking => booking.id);
 
   if (!guestBookingsIds.includes(bookingId))
-    throw new Error('Your are not allowed to edit this booking');
+    throw new Error('Your are not allowed to update this booking');
+
+  const updateData = {
+    numGuests: Number(formData.get('numGuests')),
+    observations: formData.get('observations').slice(0,1000)
+  }
+
 
   const { error } = await supabase
     .from('bookings')
-    .update(updatedFields)
-    .eq('id', id)
+    .update(updateData)
+    .eq('id', bookingId)
     .select()
     .single();
 
-  if (error) {
-    console.error(error);
-    throw new Error('Booking could not be updated');
-  }
+  if (error) throw new Error('Booking could not be updated');
+  
 
-  revalidatePath('/account/reservations/edit');
+  redirect('/account/reservations');
 }
 
 export async function deleteReservation(bookingId) {
@@ -71,6 +78,8 @@ export async function deleteReservation(bookingId) {
 
   revalidatePath('/account/reservations');
 }
+
+
 
 export async function signInAction() {
   await signIn('google', { redirectTo: '/account' });
