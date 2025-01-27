8fa83a7db89eb6d9b046b5f1e86f74e9b671a580
Paiwast <135978194+Pa1wast@users.noreply.github.com>
2025-01-27 19:57:57 +0300

    Revert "update image position"
    This reverts commit dbafc3a55f876ccd8493d9170529845a94a9272c.

--- app/page.js
+++ app/page.js
@@ -1,13 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import bg from '@/public/bg.png';
-import { getCabins } from './_lib/data-service';
 
-export default async function Page() {
-  const data = await getCabins();
-
-  console.log({ data });
-
+export default function Page() {
   return (
     <main className="mt-24">
       <Image
