0e0b1a649ef412dae81fbba3e0f6b541fe87ad3e
pa1wast <paiwastmain@gmail.com>
2025-08-07 01:41:15 +0300

    Update supabase.js

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,3 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
